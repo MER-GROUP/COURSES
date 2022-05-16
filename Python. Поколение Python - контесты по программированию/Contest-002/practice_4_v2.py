9de40369556e53770696f889c32088db7402f36e
Maxim Ramanenka (Red Alert) <i@mer-group.ru>
2022-05-16 15:01:05 +0300

    continue practice_4

--- Python. Поколение Python - контесты по программированию/Contest-002/practice_4_v2.py
+++ Python. Поколение Python - контесты по программированию/Contest-002/practice_4_v2.py
@@ -49,24 +49,22 @@ Sample Output 3:
 нечетное число раз и расположить по центру, как например в строке 
 abbba, букв a - четное количество, букв b - нечетное количество. 
 '''
-
+# авторское решение
 class Algo:
     def __init__(self, s) -> None:
         self.__algo(s)
 
     def __algo(self, s):
-        my_dict = dict()
-        for i in s:
-            my_dict[i] = my_dict.get(i, 0) + 1
-        step = int()
-        res = int()
-        for i in my_dict.values():
-            res += 1
-            if 2 == step:
-                break
-            if i % 2:
-                step += 1
-        print(res)
+        import collections
+        dict1 = collections.Counter(s)
+        flag, result = False, 0
+        for key in dict1:
+            if dict1[key] % 2 == 1:
+                flag = True
+            result += dict1[key] - dict1[key] % 2
+        if flag:
+            result += 1
+        print(result)
 
 if __name__ == '__main__':
     Algo(input())
