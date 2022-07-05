6b87d49206f18f9f33e418ebccc20ae51fa978e0
Maxim Ramanenka (Red Alert) <i@mer-group.ru>
2022-07-05 23:29:25 +0300

    fin dz9

--- Linux. Введение в Linux/Модуль 3/zd9.sh
+++ Linux. Введение в Linux/Модуль 3/zd9.sh
@@ -1,6 +1,8 @@
 #!bin/bash
 # задача на функции
 # калькулятор
+# господа, у кого возникли трудности с "*" и "**" попробуйте передавать функции значения вот таким образом:
+# calc "${a}" "${b}" "${c}"
 
 calc () { # int digit, operand, int digit
     # let "a = $1" # error
@@ -9,7 +11,7 @@ calc () { # int digit, operand, int digit
     a=$1; op=$2; b=$3
     case $op in
         "-")
-            let "res = a + b"
+            let "res = a - b"
             echo "$res"
             ;;
         "+")
@@ -38,8 +40,18 @@ calc () { # int digit, operand, int digit
     esac
 }
 
-calc $1 $2 $3
-echo "a = $a"
-echo "op = $op"
-echo "b = $b"
-echo "res = $res"
+while true; do
+    read digit1 operand digit2
+    if [[ "exit" == $digit1 ]]; then echo "bye"; break; fi
+    # calc $digit1 $operand $digit2 # error
+    # calc ${digit1} ${operand} ${digit2} # error
+    calc "${digit1}" "${operand}" "${digit2}"
+    if [[ "error" == $res ]]; then break; fi
+done
+
+# calc $1 $2 $3 # error
+# calc "${1}" "${2}" "${3}" # error
+# echo "a = $a"
+# echo "op = $op"
+# echo "b = $b"
+# echo "res = $res"
