70ad69a1fe6397e98a0c5edc9313aaf95997c59f
Red Alert aka Max Ramanenka <mer-group@yandex.ru>
2022-07-29 19:27:20 +0300

    add authors.txt

---
+++ Linux. Введение в Linux/Модуль 3/gnuplot.sh
@@ -0,0 +1,6 @@
+#!bin/bash
+sudo apt install gnuplot -y # driven graphing utility
+sudo apt install gnuplot-x11 -y # driven graphing utility
+gnuplot # setting driven graphing utility
+set terminal xterm # setting driven graphing utility
+exit # setting driven graphing utility
