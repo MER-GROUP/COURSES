bd35fa996e9ae5e75757b6588442b924a71035f1
Maxim Ramanenka (Red Alert) <i@mer-group.ru>
2022-09-07 12:09:04 +0300

    add work001

--- Python. Поколение Python - курс для продвинутых/Модуль 11/init.py
+++ Python. Поколение Python - курс для продвинутых/Модуль 11/init.py
@@ -1,1 +1,35 @@
 # Функции
+def append(element, seq=[]):
+    seq.append(element)
+    return seq
+
+print('---------------------------')
+print(append(10, [1, 2, 3]))
+print(append(5, [1]))
+print(append(1, []))
+print(append(3, [4, 5]))
+
+print('---------------------------')
+print('Значение по умолчанию', append.__defaults__)
+print(append(10))
+print('Значение по умолчанию', append.__defaults__)
+print(append(5))
+print('Значение по умолчанию', append.__defaults__)
+print(append(1))
+print('Значение по умолчанию', append.__defaults__)
+
+print('---------------------------')
+def append(element, seq=None):
+    if seq is None:
+        seq = []
+    seq.append(element)
+    return seq
+
+print('---------------------------')
+print('Значение по умолчанию', append.__defaults__)
+print(append(10))
+print('Значение по умолчанию', append.__defaults__)
+print(append(5))
+print('Значение по умолчанию', append.__defaults__)
+print(append(1))
+print('Значение по умолчанию', append.__defaults__)
