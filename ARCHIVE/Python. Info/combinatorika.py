5590282d05cb60255ec3dff0d7c651e64cf0f473
Maxim Ramanenka (Red Alert) <i@mer-group.ru>
2022-10-29 17:34:24 +0300

    fix combinatorika

--- ARCHIVE/Python. Info/combinatorika.py
+++ ARCHIVE/Python. Info/combinatorika.py
@@ -1,16 +1,24 @@
 import itertools as it
 
-# seq = [1, 2, 3, 4, 6]
-seq = '12346'
+seq = [1, 2, 3, 4, 6]
+# seq = [4,2,5,2,7,3,9,3,6,2,3,43,1,44,123,1]
+# seq = '12346'
+# seq = '46''
 
 # for i in it.accumulate(seq):
 	
 check = False
-for i in it.permutations(seq):
+for i in it.permutations(''.join(map(str,seq))):
 	for j in it.accumulate(i):
 		x = sum(map(int, j))
 		y = sum(map(int, i[len(j) :]))
 		print('x = ', x)
 		print('y = ', y)
-		print(j)
-	print(i)
+		if (x == y):
+			print('True')
+			check = True
+			break
+		# print(j)
+	# print(i)
+	if check: break
+else: print('False')
