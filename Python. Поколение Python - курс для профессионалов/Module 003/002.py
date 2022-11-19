8920636087eded97da1aa17062dde8a1f34b0ba3
Maxim Ramanenka (Red Alert) <i@mer-group.ru>
2022-11-19 09:52:38 +0300

    add 002-003 py-proff m3

--- Python. Поколение Python - курс для профессионалов/Module 003/002.py
+++ Python. Поколение Python - курс для профессионалов/Module 003/002.py
@@ -17,4 +17,11 @@ hurricane_andrew = ____
 # выводим день недели
 print(hurricane_andrew.____)
 '''
-pass
+# импортируем тип date из модуля datetime
+from datetime import date
+
+# создаем объект, соответсвующий дате урагана
+hurricane_andrew = date(1992, 8, 24)
+
+# выводим день недели
+print(hurricane_andrew.weekday())
