cc32695736ca69b723c2eae9a429b8115b7d743c
Red Alert aka Max Ramanenka <mer-group@yandex.ru>
2022-12-04 21:58:29 +0300

    init 023 py-proff m3

--- Python. Поколение Python - курс для профессионалов/Module 003/023.py
+++ Python. Поколение Python - курс для профессионалов/Module 003/023.py
@@ -47,4 +47,69 @@ print(is_available_date(dates, some_date))
 Sample Output 3:
 False
 '''
-pass
+from datetime import datetime
+
+def is_available_date(booked_dates: list[str], date_for_booking: str):
+    dates_dict = {'one': [], 'two': []}
+    for s in booked_dates:
+        arr = s.split('-')
+        arr = list(map(lambda x: datetime.strptime(x, '%d.%m.%Y'), arr))
+        if 1 == len(arr):
+            dates_dict['one'] = arr
+        else:
+            dates_dict['two'] = arr
+    
+    my_dates_dict = {'one': [], 'two': []}
+    arr = date_for_booking.split('-')
+    arr = list(map(lambda x: datetime.strptime(x, '%d.%m.%Y'), arr))
+    if 1 == len(arr):
+        my_dates_dict['one'] = arr
+    else:
+        my_dates_dict['two'] = arr
+
+    print(dates_dict) # test
+    # print(my_dates_dict) # test
+
+    # res_arr = list()
+    # if 1 == len(arr):
+    #     for date_booked in dates_dict['one']:
+    #         if my_dates_dict['one'] == date_booked:
+    #             res_arr.append(False)
+    #         else:
+    #             res_arr.append(True)
+    #     for date_booked_1, date_booked_2 in dates_dict['two']:        #############
+    #         if date_booked_1 <= my_dates_dict['one'] <= date_booked_2:
+    #             res_arr.append(False)
+    #         else:
+    #             res_arr.append(True)
+    # else:
+    #     my_dates_1, my_dates_2 = my_dates_dict['two']
+    #     for date_booked in dates_dict['one']:
+    #         if my_dates_1 <= date_booked <= my_dates_2:
+    #             res_arr.append(False)
+    #         else:
+    #             res_arr.append(True)
+    #     for date_booked_1, date_booked_2 in dates_dict['two']:        ############
+    #         if my_dates_1 <= date_booked_1 <= date_booked_2 <= my_dates_2:
+    #             res_arr.append(False)
+    #         else:
+    #             res_arr.append(True)
+
+    # return all(res_arr)
+
+if __name__ == '__main__':
+    dates = ['04.11.2021', '05.11.2021-09.11.2021']
+    some_date = '01.11.2021'
+    print(is_available_date(dates, some_date))
+
+    dates = ['04.11.2021', '05.11.2021-09.11.2021']
+    some_date = '01.11.2021-04.11.2021'
+    print(is_available_date(dates, some_date))
+
+    dates = ['04.11.2021', '05.11.2021-09.11.2021']
+    some_date = '06.11.2021'
+    print(is_available_date(dates, some_date))
+
+    dates = ['01.11.2021', '05.11.2021-09.11.2021', '12.11.2021', '15.11.2021-21.11.2021']
+    some_date = '09.11.2021'
+    print(is_available_date(dates, some_date))
