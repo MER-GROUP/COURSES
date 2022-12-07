227c766423e68cdd854c7a1c4db56dab0f377cc0
Maxim Ramanenka (Red Alert) <i@mer-group.ru>
2022-12-07 13:40:07 +0300

    add 024 py-proff m3

---
+++ Python. Поколение Python - курс для профессионалов/Module 003/025.py
@@ -0,0 +1,22 @@
+'''
+Количество дней
+Дополните приведенный ниже код, чтобы он вывел 
+количество дней (целое число) между датами today и birthday.
+
+from datetime import date, timedelta
+
+today = date(2021, 11, 4)
+birthday = date(2022, 10, 6)
+
+days = ____
+
+print(days)
+'''
+from datetime import date, timedelta
+
+today = date(2021, 11, 4)
+birthday = date(2022, 10, 6)
+
+days = ____
+
+print(days)
