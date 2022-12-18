7ecfa20a922ddc9eb2c8d9fed49ad9c2f9c09847
Red Alert aka Max Ramanenka <mer-group@yandex.ru>
2022-12-18 22:10:26 +0300

    add 049 task m2.4

--- Практикум по программированию/Module-002/049.py
+++ Практикум по программированию/Module-002/049.py
@@ -8,5 +8,25 @@
 
 Выходные данные
 Выведите пары координат соседей этой клетки в порядке слева направо сверху вниз.
+
+Sample Input:
+3 3
+2 2
+Sample Output:
+1 2
+2 3
+2 1
+3 2
 '''
-pass
+x_border, y_border = map(int, input().split())
+x, y = map(int, input().split())
+
+left, up, down, right = [0] * 4
+left = (x - 1, y) if x != 1 else (0, 0)
+up = (x, y + 1) if y != y_border else (0, 0)
+down = (x, y - 1) if y != 1 else (0, 0)
+right = (x + 1, y) if x != x_border else (0, 0)
+
+for line in (left, up, down, right):
+    if not 0 in line:
+        print(*line)
