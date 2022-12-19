8311974c8c665025edaaa51f0f376f9bcd637058
Maxim Ramanenka (Red Alert) <i@mer-group.ru>
2022-12-19 13:58:05 +0300

    add 040 py-proff

--- Python. Поколение Python - курс для профессионалов/Module 003/040.py
+++ Python. Поколение Python - курс для профессионалов/Module 003/040.py
@@ -47,5 +47,74 @@ Sample Input 2:
 Сергей Романов 28.12.1994
 Sample Output 2:
 Дни рождения не планируются
+
+Sample Input 3:
+10.11.2021
+5
+Иван Петров 11.11.1995
+Петр Сергеев 12.11.1992
+Сергей Романов 13.11.1996
+Роман Григорьев 14.11.1994
+Григорий Иванов 15.11.1995
+Sample Output 3:
+Сергей Романов
+
+Sample Input 4:
+29.11.2021
+5
+Иван Петров 01.12.1995
+Петр Сергеев 29.11.1992
+Сергей Романов 30.11.1993
+Роман Григорьев 03.12.1994
+Григорий Иванов 16.07.1995
+Sample Output 4:
+Иван Петров
+
+Sample Input 5:
+29.12.2021
+4
+Иван Петров 30.12.1995
+Петр Сергеев 04.01.1997
+Сергей Романов 03.01.1994
+Маша Иванова 31.12.1996
+Sample Output 5:
+Петр Сергеев
 '''
-pass
+from datetime import datetime
+
+current_date = datetime.strptime(input(), '%d.%m.%Y').date()
+current_date_future = current_date.replace(year=current_date.year + 1)
+arr, data_base = [input().rsplit(' ', 1) for i in range(int(input()))], {}
+max_year = datetime.min.date()
+for i in arr:
+    if datetime.strptime(i[1], '%d.%m.%Y').date().month < current_date.month:
+        if 1 <= abs(datetime.strptime(i[1], '%d.%m.%Y').date().replace(year=current_date_future.year) - current_date).days <= 7:
+            if max_year < datetime.strptime(i[1], '%d.%m.%Y').date():
+                max_year = datetime.strptime(i[1], '%d.%m.%Y').date()
+            data_base.setdefault(
+                datetime.strptime(i[1], '%d.%m.%Y').date(), 
+                []
+            ).append(*i[:-1])
+    else:
+        if 1 <= abs(datetime.strptime(i[1], '%d.%m.%Y').date().replace(year=current_date.year) - current_date).days <= 7:
+            if max_year < datetime.strptime(i[1], '%d.%m.%Y').date():
+                max_year = datetime.strptime(i[1], '%d.%m.%Y').date()
+            data_base.setdefault(
+                datetime.strptime(i[1], '%d.%m.%Y').date(), 
+                []
+            ).append(*i[:-1])
+
+data_base_sorted = dict()
+for k, v in sorted(data_base.items()):
+    if max_year.year == k.year:
+        if current_date.month <= k.month:
+            data_base_sorted[k.replace(year=current_date.year)] = v
+        else:
+            data_base_sorted[k.replace(year=current_date_future.year)] = v
+
+for key in data_base_sorted.keys():
+    if 1 <= abs(key - current_date).days <= 7:
+        print(*data_base_sorted[key])
+        break
+else:
+    print('Дни рождения не планируются')
