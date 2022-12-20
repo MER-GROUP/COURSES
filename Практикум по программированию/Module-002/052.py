94b0b5f3b2bd85d920d1aebff4406a209c1aa690
Red Alert aka Max Ramanenka <mer-group@yandex.ru>
2022-12-20 21:24:20 +0300

    add 052 task m2.4

--- Практикум по программированию/Module-002/052.py
+++ Практикум по программированию/Module-002/052.py
@@ -13,4 +13,24 @@ Sample Input:
 Sample Output:
 IV
 '''
-pass
+def n_to_rome(n):
+    rome = {
+        1: 'I', 2: 'II', 3: 'III', 4: 'IV', 5: 'V', 
+        6: 'VI', 7: 'VII', 8: 'VIII', 9: 'IX', 10: 'X', 
+        20: 'XX', 30: 'XXX', 40: 'XL', 50: 'L', 60: 'LX', 
+        70: 'LXX', 80: 'LXXX', 90: 'XC', 100: 'C'
+    }
+
+    a = n // 100 * 100
+    b = n % 100 // 10 * 10
+    c = n % 10
+    res = str()
+
+    if 0 != a: res += rome[a]
+    if 0 != b: res += rome[b]
+    if 0 != c: res += rome[c]
+
+    return res
+
+if __name__ == '__main__':
+    print(n_to_rome(int(input())))
