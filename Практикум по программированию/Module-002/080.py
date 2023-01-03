281a290776f95c7f44d17292caa6a806b2fa0ae0
Red Alert aka Max Ramanenka <mer-group@yandex.ru>
2023-01-03 19:07:47 +0300

    add 080-081 task m2.7

--- Практикум по программированию/Module-002/080.py
+++ Практикум по программированию/Module-002/080.py
@@ -16,4 +16,36 @@ Sample Input:
 Sample Output:
 1 2 4 8 16 32
 '''
-pass
+n = int(input())
+i = 1
+while i <= n:
+    print(i, end=' ')
+    i += i 
+
+# print()
+# print('------')
+# print(*range(int(__import__('math').log2(n)) + 1))
+# print(1 << 0)
+# print(1 << 1)
+# print(1 << 2)
+# print(1 << 3)
+# print(1 << 4)
+# print(1 << 5)
+
+# print()
+# print('------')
+# print(int('1' + '0' * 0, 2))
+# print(int('1' + '0' * 1, 2))
+# print(int('1' + '0' * 2, 2))
+# print(int('1' + '0' * 3, 2))
+# print(int('1' + '0' * 4, 2))
+# print(int('1' + '0' * 5, 2))
+
+# print()
+# print('------')
+# print(int('1', 2))
+# print(int('10', 2))
+# print(int('100', 2))
+# print(int('1000', 2))
+# print(int('10000', 2))
+# print(int('100000', 2))
