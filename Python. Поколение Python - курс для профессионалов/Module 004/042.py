86176e50316bc451e30737c6ea2d68eab13e651b
Maxim Ramanenka (Red Alert) <i@mer-group.ru>
2023-01-27 14:21:57 +0300

    add 042 py-proff m4

--- Python. Поколение Python - курс для профессионалов/Module 004/042.py
+++ Python. Поколение Python - курс для профессионалов/Module 004/042.py
@@ -21,5 +21,25 @@ https://stepik.org/media/attachments/lesson/547172/workbook.zip
 https://stepik.org/media/attachments/lesson/547172/clue_dragonborn.txt
 '''
 from zipfile import ZipFile
+from datetime import datetime
 
-pass
+pattern = '%Y-%m-%d %H:%M:%S'
+dale_later = datetime.strptime('2021-11-30 14:22:00', pattern)
+
+# with ZipFile(file='042-workbook.zip', mode='r') as zip_opener:
+with ZipFile(file='workbook.zip', mode='r') as zip_opener:
+    files_zip = zip_opener
+
+    print(
+        *sorted(
+            map(
+                # lambda x: x.filename.split('/')[-1] if x.filename.split('/')[-1] else x.filename.split('/')[-2],
+                lambda x: x.filename.split('/')[-1],
+                filter(
+                    lambda x: datetime(*x.date_time) > dale_later and not x.is_dir(),
+                    files_zip.infolist()
+                )
+            )
+        ),
+        sep='\n'
+    )
