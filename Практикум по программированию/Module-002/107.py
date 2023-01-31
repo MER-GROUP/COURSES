5dad684538833230c2da1f78b62f3ec9822bd874
Red Alert aka Max Ramanenka <mer-group@yandex.ru>
2023-02-01 00:36:00 +0300

    add 105-106 task m2.9

---
+++ Практикум по программированию/Module-002/107.py
@@ -0,0 +1,20 @@
+'''
+Обращение числа
+
+Входные данные
+Задано единственное число N, не больше 10000.
+
+Выходные данные
+Необходимо вывести цифры данного числа в обратном порядке.
+
+Sample Input:
+48
+Sample Output:
+84
+'''
+import sys
+
+sys.stdin = open(file='107.csv', mode='rt', encoding='utf-8', newline='')
+tup = sys.stdin.read()
+
+pass
