d025cbfda7dbc3288122927195bce59a5104129f
Maxim Ramanenka (Red Alert) <i@mer-group.ru>
2023-02-03 13:14:32 +0300

    add 048 py-proff m4

--- Python. Поколение Python - курс для профессионалов/Module 004/048.py
+++ Python. Поколение Python - курс для профессионалов/Module 004/048.py
@@ -42,5 +42,32 @@ https://stepik.org/media/attachments/lesson/547172/desktop.zip
 https://stepik.org/media/attachments/lesson/547172/clue_archive_structure.txt
 '''
 from zipfile import ZipFile
+from sys import platform
 
-pass
+separator = '\\' if 'win32' == platform else '/'
+
+def convert(n: int):
+    unit = ['B', 'KB', 'MB', 'GB']
+    step = 0
+    while 1024 < n:
+        n /= 1024
+        step += 1
+    return round(n), unit[step]
+
+# with ZipFile(file='048-test.zip', mode='r') as zip_opener:
+# with ZipFile(file='048-desktop.zip', mode='r') as zip_opener:
+with ZipFile(file='desktop.zip', mode='r') as zip_opener:
+    file_in_zip = zip_opener.infolist()
+
+    for file in file_in_zip:
+        filename = file.filename
+        filename_len = len(filename.split(separator)) if not file.is_dir() else len(filename.split(separator))-1
+        
+        if 1 == filename_len and file.is_dir():
+            print(filename[:-1], *convert(file.file_size) if not file.is_dir() else '')
+        else:
+            s = str()
+            for i in range(filename_len - 1):
+                s += '  '
+            filename = s + filename.split(separator)[-2 if file.is_dir() else -1]
+            print(filename, *convert(file.file_size) if not file.is_dir() else '')
