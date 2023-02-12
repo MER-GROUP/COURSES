0e3b8681bb6992271ae3ff81226f8d041c03fe6f
Red Alert aka Max Ramanenka <mer-group@yandex.ru>
2023-02-12 23:09:16 +0300

    add 018-019 task m3.2

---
+++ Практикум по программированию/Module-003/020.py
@@ -0,0 +1,28 @@
+'''
+Удали пробелы
+Дана строка, Вам требуется преобразовать все идущие подряд пробелы в один. 
+Если пробелы есть в начале и в конце строки - их нужно убрать.
+
+Входные данные
+Длина строки не превосходит 1000.
+
+Выходные данные
+Выведите измененную строку.
+
+Sample Input:
+nz d urp lren s bwz  boom  t a   j    ho    vi
+Sample Output:
+nz d urp lren s bwz boom t a j ho vi
+'''
+import sys
+
+sys.stdin = open(file='020.csv', mode='rt', encoding='utf-8', newline='')
+arr = tuple(
+    map(
+        str.strip,
+        sys.stdin.readlines()
+    )
+)
+print(arr) # test
+
+pass
