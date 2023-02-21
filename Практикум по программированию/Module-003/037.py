a3b7b85926b74f62f7e18c75db6eb1291f43f05f
Maxim Ramanenka (Red Alert) <i@mer-group.ru>
2023-02-21 14:42:05 +0300

    add 035-036 task m3.4

---
+++ Практикум по программированию/Module-003/037.py
@@ -0,0 +1,23 @@
+'''
+Вставка символов
+Дана строка. Получите новую строку, вставив между двумя символами 
+исходной строки символ *. Выведите полученную строку.
+
+Входные данные
+Вводится строка.
+
+Выходные данные
+Выведите ответ на задачу.
+
+Sample Input:
+Python
+Sample Output:
+P*y*t*h*o*n
+'''
+import sys  
+
+sys.stdin = open(file='037.csv', mode='rt', encoding='utf-8', newline='')
+arr = sys.stdin.read()
+print(arr) # test
+
+pass
