d59687cf824ef2525398712b8833db1f2691f268
Red Alert aka Max Ramanenka <mer-group@yandex.ru>
2023-02-22 00:43:12 +0300

    add 037 task m3.4

---
+++ Практикум по программированию/Module-003/038.py
@@ -0,0 +1,22 @@
+'''
+Удалить каждый третий символ
+Дана строка. Удалите из нее все символы, чьи индексы делятся на 3.
+
+Входные данные
+Вводится строка.
+
+Выходные данные
+Выведите ответ на задачу.
+
+Sample Input:
+Python
+Sample Output:
+yton
+'''
+import sys  
+
+sys.stdin = open(file='038.csv', mode='rt', encoding='utf-8', newline='')
+arr = sys.stdin.read()
+print(arr) # test
+
+pass
