e7d9bfbff8158d7d5f9d2bfe8a6f576c5cb53a7a
Maxim Ramanenka (Red Alert) <i@mer-group.ru>
2023-02-24 13:56:18 +0300

    add 007-009 py-proff m6

--- Python. Поколение Python - курс для профессионалов/Module 006/init-6.py
+++ Python. Поколение Python - курс для профессионалов/Module 006/init-6.py
@@ -1,5 +1,32 @@
 print('------------------')
 
-pass
+from collections import defaultdict
+
+red_1 = defaultdict(int, name='Timur', surname='Guev', hobby='math')
+print(red_1)
+
+red_2 = defaultdict(name='Timur', surname='Guev', hobby='math')
+print(red_2)
+
+red_3 = defaultdict(int, {'name': 'Timur', 'surname': 'Guev', 'hobby': 'math'})
+print(red_3)
+
+red_4 = defaultdict()
+print(red_4)
+
+red_5 = defaultdict(list)
+print(red_5)
+
+red_6 = defaultdict(int, [('name', 'Timur'), ('surname', 'Guev'), ('hobby', 'math')])
+print(red_6)
+
+# red_7 = defaultdict({'name': 'Timur', 'surname': 'Guev', 'hobby': 'math'})
+# print(red_7)
+
+red_8 = defaultdict(None)
+print(red_8)
+
+red_9 = defaultdict.fromkeys(['name', 'surname', 'hobby'], None)
+print(red_9)
 
 print('------------------')
