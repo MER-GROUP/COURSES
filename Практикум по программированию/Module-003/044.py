d11eb2a1571499a052b69719e1a64240817455aa
Red Alert aka Max Ramanenka <mer-group@yandex.ru>
2023-02-28 04:07:03 +0300

    init 044 task m3.5

--- Практикум по программированию/Module-003/044.py
+++ Практикум по программированию/Module-003/044.py
@@ -26,15 +26,49 @@ from collections import namedtuple
 coords = namedtuple('coords', ('x', 'y'))
 from math import hypot
 
-def round_10(n: float) -> float:
-    start, end = str(n).split('.')
-    return float(start + '.' + end[:12])
+def sorted_sides(a: coords, b: coords, c: coords) -> bool:
+    pass
+
+def is_triangle(a: coords, b: coords, c: coords) -> bool:
+    # sides of triangle
+    ab, bc, ca = sorted(
+        (
+            hypot(a.x - b.x, a.y - b.y),
+            hypot(b.x - c.x, b.y - c.y),
+            hypot(c.x - a.x, c.y - a.y)
+        )
+    )
+    return (True, False)[ca > ab + bc]
+
+def is_all_on_same_line(a: coords, b: coords, c: coords) -> bool:
+    return (False, True)[a.x == b.x == c.x or a.y == b.y == c.y]
+
+def heron(a: coords, b: coords, c: coords) -> float:
+    # sides of triangle
+    ab, bc, ca = sorted(
+        (
+            hypot(a.x - b.x, a.y - b.y),
+            hypot(b.x - c.x, b.y - c.y),
+            hypot(c.x - a.x, c.y - a.y)
+        )
+    )
+    p = (ab + bc + ca) / 2
+    return (p * (p - ab) * (p - bc) * (p - ca))**0.5
 
 sys.stdin = open(file='044.csv', mode='rt', encoding='utf-8', newline='')
 _, *arr = map(str.strip, sys.stdin.read().splitlines())
 # print(*arr, sep='\n') # test
 
 arr = tuple(coords._make(map(int, el.split())) for el in arr)
-print(*arr) # test
+# print(*arr) # test
 
-pass
+s_max = float('-inf')
+for i in range(len(arr)-2):
+    for j in range(i+1, len(arr)-1):
+        for k in range(j+1, len(arr)):
+            # coordinates of the triangle
+            a, b, c = arr[i], arr[j], arr[k]
+            # if triangle exists and not all sides are on same line
+            if is_triangle(a, b, c) and not is_all_on_same_line(a, b, c):
+                s_max = max(heron(a, b, c), s_max)
+print(round(s_max, 1))
