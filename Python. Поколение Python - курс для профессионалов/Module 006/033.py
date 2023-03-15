1f44fc3a582c0917e7278df1cf0fa0c98d132206
Max Ramanenka (Red Alert) <i@mer-group.ru (mer-group@yandex.ru)>
2023-03-15 15:02:15 +0300

    add init-10 py-proff m6

---
+++ Python. Поколение Python - курс для профессионалов/Module 006/033.py
@@ -0,0 +1,39 @@
+'''
+Зоопарк
+Вам доступен файл zoo.json, содержащий список JSON-объектов с данными об обитателях 
+некоторого зоопарка. Ключом в каждом объекте является название животного, 
+значением — их количество в зоопарке:
+
+[
+   {
+      "Axolotl": 11,
+      "Poison Frog": 12,
+      "Sonoran Toad": 6,
+      "Tiger Salamander": 16
+   },
+   {
+      "African Fish Eagle": 6,
+      "Andean Condor": 8,
+      "Black Vulture": 8,
+      "Bufflehead Duck": 9,
+      "Flamingo": 8,
+      "Great Horned Owl": 16,
+      "Hornbill": 1
+   },
+   ...
+]
+
+Напишите программу, которая определяет, сколько всего животных обитает в зоопарке, 
+и выводит полученный результат.
+
+Примечание 1. Гарантируется, что все ключи в JSON-объектах, различны.
+
+Примечание 2. Указанный файл доступен по ссылке. Ответ на задачу доступен по ссылке.
+https://stepik.org/media/attachments/lesson/634670/zoo.json
+https://stepik.org/media/attachments/lesson/634670/clue_zoo.txt
+
+Примечание 3. При открытии файла используйте явное указание кодировки UTF-8.
+'''
+from collections import ChainMap
+
+pass
