4eb2325e510ae5be6f4a5e816215a9797fcba664
Max Ramanenka (Red Alert) <i@mer-group.ru (mer-group@yandex.ru)>
2023-03-17 15:24:39 +0300

    init 059 task m3.6

--- Практикум по программированию/Module-003/059.py
+++ Практикум по программированию/Module-003/059.py
@@ -36,10 +36,41 @@ YES
 import sys
 import decimal as d
 
-sys.stdin = open(file='059.csv', mode='rt', encoding='utf-8', newline='')
+# sys.stdin = open(file='059.csv', mode='rt', encoding='utf-8', newline='')
 _xyz, _, *_abcq = tuple(map(str.strip, sys.stdin.read().splitlines()))
-print(_xyz) # test
-print(_) # test
-print(_abcq) # test
+# print(_xyz) # test
+# print(_) # test
+# print(_abcq) # test
 
-pass
+_list_xyz = list()
+for i in map(d.Decimal, _xyz.split()):
+    _list_xyz.append(i)
+
+_list_abcq = list()
+for _str in _abcq:
+    _arr = tuple(map(d.Decimal,_str.split()))
+    _list_abcq.append(sum(_arr[:-1]) * _arr[-1])
+
+x, y, z = map(lambda x, y: x - y, _list_xyz, _list_abcq)
+if (x<=0) and (y<=0) and (z<=0):
+    print('YES') 
+else:
+    print('NO')
+# print(('NO', "YES")[all(map(lambda x, y: x - y <= 0, _list_xyz, _list_abcq))])
+
+"""
+var
+  x,y,z,a,b,c,q:real;
+  i:integer;
+begin
+  readln(x,y,z);
+  readln(i);
+  for i:=1 to i do begin
+    readln(a,b,c,q);
+    x:=x-a*q;
+    y:=y-b*q;
+    z:=z-c*q;
+  end;
+  if (x<=0) and (y<=0) and (z<=0) then writeln('YES') else writeln('NO');
+end.
+"""
