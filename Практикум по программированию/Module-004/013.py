fa46a6e68679ad25cc65a66a17efe0112a5ae413
Max Ramanenka (Red Alert) <i@mer-group.ru (mer-group@yandex.ru)>
2023-03-24 18:48:16 +0300

    add 013 task m4.1

--- Практикум по программированию/Module-004/013.py
+++ Практикум по программированию/Module-004/013.py
@@ -2,7 +2,8 @@
 Двойной переворот
 Дана последовательность натуральных чисел 1, 2, 3, ..., N (1 ≤ N ≤ 1000). 
 Необходимо сначала расположить в обратном порядке часть этой последовательности 
-от элемента с номером A до элемента с номером B, а затем от C до D (A < B; C < D; 1 ≤ A, B, C, D ≤ N).
+от элемента с номером A до элемента с номером B, а затем от C до D 
+(A < B; C < D; 1 ≤ A, B, C, D ≤ N).
 
 Входные данные
 Вводятся натуральные числа числа N, A, B, C, D.
@@ -18,9 +19,50 @@ Sample Output:
 import sys
 from array import array
 
-sys.stdin = open(file='013.csv', mode='rt', encoding='utf-8', newline='')
+# sys.stdin = open(file='013.csv', mode='rt', encoding='utf-8', newline='')
 arr= tuple(map(str.strip, sys.stdin.read().splitlines()))
 arr = array('i', list(map(int, arr[0].split())))
-print(arr) # test
+# print(arr) # test
 
-pass
+N, A, B, C, D = arr
+A -= 1; B -= 1; C -= 1; D -= 1
+# print(N, A, B, C, D)
+
+# 1
+print(
+    *list(range(1, N+1))[:A],
+    *list(range(1, N+1))[A:B+1][::-1],
+    *list(range(1, N+1))[B+1:C],
+    *list(range(1, N+1))[C:D+1][::-1],
+    *list(range(1, N+1))[D+1:]
+)
+
+# 2
+_arr = array('i', range(1, N+1))
+# print(_arr) # test
+j = B
+for i in range(A, B+1):
+    if i == j or i > j: break
+    _arr[i], _arr[j] = _arr[j], _arr[i]
+    j -= 1
+# print(_arr) # test
+j = D
+for i in range(C, D+1):
+    if i == j or i > j: break
+    _arr[i], _arr[j] = _arr[j], _arr[i]
+    j -= 1
+# print(_arr) # test
+print(*_arr)
+
+# 3
+_arr = array('i', range(1, N+1))
+# print(_arr) # test
+for i, j in zip(range(A, B+1), range(B, A-1, -1)):
+    if i == j or i > j: break
+    _arr[i], _arr[j] = _arr[j], _arr[i]
+# print(_arr) # test
+for i, j in zip(range(C, D+1), range(D, C-1, -1)):
+    if i == j or i > j: break
+    _arr[i], _arr[j] = _arr[j], _arr[i]
+# print(_arr) # test
+print(*_arr)
