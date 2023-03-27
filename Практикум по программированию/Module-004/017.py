dd3ed5c44a0365e26055e8e7cc1084a6809887b8
Max Ramanenka (Red Alert) <i@mer-group.ru (mer-group@yandex.ru)>
2023-03-27 15:04:27 +0300

    add 016-020 task m4.2

--- Практикум по программированию/Module-004/017.py
+++ Практикум по программированию/Module-004/017.py
@@ -1,8 +1,6 @@
 '''
-Четные индексы
-Выведите все элементы списка с четными индексами (то есть A[0], A[2], A[4], ...).
-
-Программа должна быть эффективной и не выполнять лишних действий!
+Четные элементы
+Выведите все четные элементы списка.
 
 Входные данные
 Вводится список чисел. Все числа списка находятся на одной строке.
@@ -11,16 +9,16 @@
 Выведите ответ на задачу.
 
 Sample Input:
-1 2 3 4 5
+1 2 2 3 3 3 4
 Sample Output:
-1 3 5
+2 2 4
 '''
 import sys
 from array import array
 
-# sys.stdin = open(file='016.csv', mode='rt', encoding='utf-8', newline='')
+# sys.stdin = open(file='017.csv', mode='rt', encoding='utf-8', newline='')
 arr = tuple(map(str.strip, sys.stdin.read().splitlines()))
 arr = array('i', list(map(int, arr[0].split())))
 # print(arr) # test
 
-print(*arr[::2])
+print(*filter(lambda x: not(x % 2), arr))
---
+++ Практикум по программированию/Module-004/021.py
@@ -0,0 +1,30 @@
+'''
+Больше своих соседей
+Дан список чисел. Определите, сколько в этом списке элементов, 
+которые больше двух своих соседей и выведите количество таких элементов.
+
+Входные данные
+Вводится список чисел. Все числа списка находятся на одной строке.
+
+Выходные данные
+Выведите ответ на задачу.
+
+Sample Input 1:
+1 2 3 4 5
+Sample Output 1:
+0
+
+Sample Input 2:
+1 5 1 5 1
+Sample Output 2:
+2
+'''
+import sys
+from array import array
+
+sys.stdin = open(file='021.csv', mode='rt', encoding='utf-8', newline='')
+arr = tuple(map(str.strip, sys.stdin.read().splitlines()))
+arr = array('i', list(map(int, arr[0].split())))
+print(arr) # test
+
+pass
