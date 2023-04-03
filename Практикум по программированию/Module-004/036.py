dc016ed0bc81cd725aaa34d42f94bdb9f138d5aa
Max Ramanenka (Red Alert) <i@mer-group.ru (mer-group@yandex.ru)>
2023-04-03 21:07:45 +0300

    add 036 task m4.3

--- Практикум по программированию/Module-004/036.py
+++ Практикум по программированию/Module-004/036.py
@@ -18,10 +18,32 @@ Sample Output:
 '''
 import sys
 from array import array
+from collections import Counter, defaultdict
 
-sys.stdin = open(file='036.csv', mode='rt', encoding='utf-8', newline='')
+# sys.stdin = open(file='036.csv', mode='rt', encoding='utf-8', newline='')
 arr = tuple(map(str.strip, sys.stdin.read().splitlines()))
 arr = array('i', list(map(int, arr[0].split())))
-print(arr) # test
+# print(arr) # test
 
-pass
+print(Counter(arr).most_common()[0][0]) # 1
+
+# 2
+_dict = dict()
+for _el in arr:
+    _dict[_el] = _dict.get(_el, 0) + 1
+print(max(_dict, key=_dict.get))
+
+# 3
+_defaultdict = defaultdict(int)
+for _el in arr:
+    _defaultdict[_el] += 1
+print(max(_dict, key=_defaultdict.get))
+
+# 4
+_max = float('-inf')
+_number = int()
+for _el in arr:
+    if _max < arr.count(_el):
+        _max = arr.count(_el)
+        _number = _el
+print(_el)
