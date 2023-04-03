a404c9ea792879c9174ed87b32fb3744b8cc352b
Max Ramanenka (Red Alert) <i@mer-group.ru (mer-group@yandex.ru)>
2023-04-03 15:20:01 +0300

    add init-2 and 001-003 py-proff m8

--- Python. Поколение Python - курс для профессионалов/Module 008/init-2.py
+++ Python. Поколение Python - курс для профессионалов/Module 008/init-2.py
@@ -1,5 +1,55 @@
 print('------------------')
 
-pass
+def draw_rect(width, height, step):
+    if step < height:
+        print('*' * width)
+        draw_rect(width, height, step + 1)
+
+draw_rect(4, 3, 0)
+print()
+draw_rect(6, 6, 0)
+print()
+draw_rect(10, 2, 0)
+
+print('------------------')
+
+def draw_rect(width, height, step=0):
+    if step < height:
+        print('*' * width)
+        draw_rect(width, height, step + 1)
+
+draw_rect(4, 3)
+print()
+draw_rect(6, 6)
+print()
+draw_rect(10, 2)
+
+print('------------------')
+
+def draw_rect(width, height):
+    def rec(step):
+        if step < height:
+            print('*' * width)
+            rec(step + 1)
+    rec(0)
+
+draw_rect(4, 3)
+print()
+draw_rect(6, 6)
+print()
+draw_rect(10, 2)
+
+print('------------------')
+
+def print_numbers(start, end):
+    def rec(num):
+        if num <= end:
+            print(num)
+            rec(num + 1)
+    rec(start)
+
+print_numbers(0, 7)
+print()
+print_numbers(-3, 4)
 
 print('------------------')
