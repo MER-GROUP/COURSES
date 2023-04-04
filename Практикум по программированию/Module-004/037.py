a59daa72e4e63a0f19d80018f4c3fab4db67c718
Max Ramanenka (Red Alert) <i@mer-group.ru (mer-group@yandex.ru)>
2023-04-04 20:24:40 +0300

    add 037 task m4.3

--- Практикум по программированию/Module-004/037.py
+++ Практикум по программированию/Module-004/037.py
@@ -19,10 +19,47 @@ Sample Output:
 '''
 import sys
 from array import array
+from copy import copy
 
 sys.stdin = open(file='037.csv', mode='rt', encoding='utf-8', newline='')
 arr = tuple(map(str.strip, sys.stdin.read().splitlines()))
 arr = array('i', list(map(int, arr[0].split())))
 print(arr) # test
 
-pass
+# 1
+_arr = copy(arr)
+_len = len(_arr)
+_i = 0
+while 0 < _len:
+    if 0 == _arr[_i]:
+        _arr.append(_arr.pop(_i))
+    else:
+        _i += 1
+    _len -= 1
+print(*_arr)
+
+# 2
+_arr2 = copy(arr)
+_count = 0
+_i = 0
+while _i < len(_arr2):
+    if 0 == _arr2[_i]:
+        del _arr2[_i]
+        _count += 1
+        continue
+    _i += 1
+_arr2.extend([0]*_count)
+print(*_arr2)
+
+# 3
+lst = copy(arr)
+zpos = 0
+for i in range(len(lst)):
+    if lst[i]:
+        lst[zpos], lst[i] = lst[i], lst[zpos]
+        zpos += 1
+print(*lst)  
+
+# 4
+lst2 = copy(arr)
+print(*sorted(lst2, key=lambda x: x == 0))
