6d9a3a7a922b9d615a1f0933d2b1936af3f1af74
Max Ramanenka (Red Alert) <i@mer-group.ru (mer-group@yandex.ru)>
2023-04-15 14:46:44 +0300

    add 046 task m4.4

--- Практикум по программированию/Module-004/046.py
+++ Практикум по программированию/Module-004/046.py
@@ -25,13 +25,26 @@ import sys
 # from array import array
 # from copy import copy
 
-sys.stdin = open(file='046.csv', mode='rt', encoding='utf-8', newline='')
+# sys.stdin = open(file='046.csv', mode='rt', encoding='utf-8', newline='')
 nm, *arr = tuple(map(str.strip, sys.stdin.read().splitlines()))
 # arr = array('i', list(map(int, arr.split())))
 n, m = map(int, nm.split())
-print(n) # test
-print(m) # test
+# print(n) # test
+# print(m) # test
 arr = [list(map(int, arr[i].split())) for i in range(n)]
-[print(*i) for i in arr]
+# [print(*i) for i in arr]
 
-pass
+_max = float('-inf')
+_count = 0
+_i_prev = None
+for i in range(n):
+    for j in range(m):
+        if _max < arr[i][j]:
+            _max = arr[i][j]
+            _count = 1
+            _i_prev = i
+        elif _max == arr[i][j]:
+            if not i == _i_prev:
+                _count += 1
+                _i_prev = i
+print(_count)
