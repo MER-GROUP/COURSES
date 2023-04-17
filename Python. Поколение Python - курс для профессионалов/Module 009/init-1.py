fa3d02ea146e9752e0309a0d54c395f2aeb6a4e1
Max Ramanenka (Red Alert) <i@mer-group.ru (mer-group@yandex.ru)>
2023-04-17 13:16:20 +0300

    add 001-002 py-proff m9

--- Python. Поколение Python - курс для профессионалов/Module 009/init-1.py
+++ Python. Поколение Python - курс для профессионалов/Module 009/init-1.py
@@ -1,5 +1,118 @@
 print('------------------')
 
-pass
+print(abs(-19))
+print(abs(3.14))
+print(abs(3 + 4j))
+
+print('------------------')
+
+print(bin(13))
+
+print('------------------')
+
+print(hex(63))
+
+print('------------------')
+
+print(oct(44))
+
+print('------------------')
+
+print(round(3.4))
+print(round(3.7))
+print(round(3.48, 1))
+print(round(3.41, 1))
+print(round(3.376, 2))
+print(round(3.371, 2))
+print(round(2.5))
+print(round(3.5))
+
+print('------------------')
+
+print(round(3.5))
+print(round(4.5))
+
+print('------------------')
+
+print(pow(3, 4))
+print(pow(2, 5, 30))
+print(divmod(81, 30))
+
+print('------------------')
+
+print(int('37'))
+print(int('111', base=2))
+print(int(19.7))
+print(int())
+
+print('------------------')
+
+print(float('-13.99'))
+print(float(78))
+print(float())
+print(float('inf'))
+print(float('-inf'))
+
+print('------------------')
+
+print(complex(4, 9))
+print(complex('11+5j'))
+
+print('------------------')
+
+print(bool('Beegeek'))
+print(bool(17))
+print(bool(['apple', 'cherry']))
+print(bool())
+print(bool(''))
+print(bool(0))
+print(bool([]))
+
+print('------------------')
+
+print(str(11))
+print(str(set(range(5))))
+
+print('------------------')
+
+print(list())
+print(list(range(5)))
+print(list('abcde'))
+print(list([1, 2, 3, 4]))
+
+print('------------------')
+
+print(tuple())
+print(tuple('stepik'))
+print(tuple([1, 2, 3, 4]))
+
+print('------------------')
+
+print(set())
+print(set(map(int, '12345')))
+print(set(['a', 'b', 'a', 'd', 'd']))
+
+print('------------------')
+
+print(frozenset())
+print(frozenset('beegeek'))
+print(frozenset(set('aaaabbccccccde')))
+
+print('------------------')
+
+print(dict())
+print(dict(a=1, b=2))
+print(dict([('a', 1), ('b', 2), ('c', 3)]))
+print(dict([('a', 1), ('b', 2)], c=3, d=4))
+
+print('------------------')
+
+print(ord('a'))
+print(ord('B'))
+
+print('------------------')
+
+print(chr(97))
+print(chr(66))
 
 print('------------------')
