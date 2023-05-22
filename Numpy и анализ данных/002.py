03390e4c89af4d05e9d4bf986a708ce427b1e6a4
Max Ramanenka (Red Alert) <i@mer-group.ru (mer-group@yandex.ru)>
2023-05-22 19:42:49 +0300

    init-2 002 numpy m3.1

--- Numpy и анализ данных/002.py
+++ Numpy и анализ данных/002.py
@@ -1,3 +1,23 @@
+print('-------------------------')
+
 import numpy as np
 
-pass
+my_list = [1, 2, 3, 4, 5]
+my_array = np.array(my_list)
+print(my_array)
+print(type(my_array))
+print(my_list)
+print(type(my_list))
+
+print('-------------------------')
+
+import numpy as np
+
+zeros_array = np.zeros(5) 
+ones_array = np.ones(5)
+random_array = np.random.rand(5)
+print(zeros_array)
+print(ones_array)
+print(random_array)
+
+print('-------------------------')
