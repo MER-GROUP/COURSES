82d7e36d8f3184bb1b8fc7bf374dd467da9e027a
Max Ramanenka (Red Alert) <i@mer-group.ru (mer-group@yandex.ru)>
2023-05-28 10:44:46 +0300

    add 006 pattern decorator

--- Pattern. Python/006.py
+++ Pattern. Python/006.py
@@ -1,7 +1,39 @@
 '''
+Это упражнение на написание декоратора.
 
+Напишите декоратор flip, который делает так, 
+что задекорированная функция принимает все свои неименованные 
+аргументы в порядке, обратном тому, в котором их передали 
+(для аргументов с именем не вполне правильно учитывать порядок, 
+в котором они были переданы)
+
+def flip('...'):
+    '...'
+
+
+Примеры ожидаемого поведения﻿
+
+@flip
+def div(x, y, show=False):
+    res = x / y
+    if show:
+        print(res)
+    return res
+    
+div(2, 4, show=True)
+>>> 2.0
 '''
-pass
+def flip(func):
+    def wrapper(*args, **kwargs):
+        return func(*args[::-1], **kwargs)
+    return wrapper
+
+@flip
+def div(x, y, show=False):
+    res = x / y
+    if show:
+        print(res)
+    return res
 
 if __name__ == '__main__':
-    pass
+    div(2, 4, show=True)
