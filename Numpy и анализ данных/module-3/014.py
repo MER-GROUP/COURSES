9081545024855a84c80d35d96a62fa533835ab38
Max Ramanenka (Red Alert) <i@mer-group.ru (mer-group@yandex.ru)>
2023-05-30 14:31:26 +0300

    add 011-013 numpy m3.2

---
+++ Numpy и анализ данных/module-3/014.py
@@ -0,0 +1,13 @@
+'''
+N единиц
+На вход подается целое число n>0, cоздайте одномерный массив, 
+состоящий из n единиц, и выведите его на экран.
+
+Sample Input:
+2
+Sample Output:
+[1. 1.]
+'''
+import numpy as np
+
+print(np.ones(int(input()), dtype=float))
