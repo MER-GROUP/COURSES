945e0e646cd1ab3db8b4c3f9a03195553248dbf8
Max Ramanenka (Red Alert) <i@mer-group.ru (mer-group@yandex.ru)>
2023-06-05 20:05:01 +0300

    add 014 pattern decorator

--- Pattern. Python/decorator/014.py
+++ Pattern. Python/decorator/014.py
@@ -1,9 +1,40 @@
 '''
+Это упражнение на написание декоратора.
 
+Определите новую версию декоратора apply, которая может 
+принимать аргументы, и, соответственно, при декорировании 
+некоторой функции apply установит переменную с именем 
+задекорированной функции в значение этой функции на 
+переданных apply аргументах.
+
+def apply('...'):
+    '...'
+
+Примеры ожидаемого поведния
+
+@apply(2, 3)
+def multiply(x, y):
+    return x * y
+    
+print(multiply, type(multiply))
+ 
+>>> 6, <class 'int'>
 '''
 from functools import wraps, partial
 
-pass
+def apply(*argsz, **kwargsz):
+    def new_apply(func):
+        if argsz or kwargsz:
+            return func(*argsz, **kwargsz)  
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            return func(*args, **kwargs)
+        return wrapper
+    return new_apply
 
 if __name__ == '__main__':
-    pass
+    @apply(2, 3)
+    def multiply(x, y):
+        return x * y
+        
+    print(multiply, type(multiply))
