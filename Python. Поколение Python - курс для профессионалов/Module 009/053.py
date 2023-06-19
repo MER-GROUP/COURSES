790b73f767a4ae5aa95d1366b43802e022923c5e
Max Ramanenka (Red Alert) <i@mer-group.ru (mer-group@yandex.ru)>
2023-06-19 13:36:54 +0300

    add 053 py-proff m9

--- Python. Поколение Python - курс для профессионалов/Module 009/053.py
+++ Python. Поколение Python - курс для профессионалов/Module 009/053.py
@@ -1,9 +1,65 @@
 '''
+Две функции
+Вам доступна уже реализованная функция send_email(), которая принимает три аргумента 
+в следующем порядке:
 
+name — имя
+email_address — адрес электронной почты
+text — содержание письма
+
+Функция отправляет письмо пользователю с именем name на адрес email_address с содержанием text.
+
+1. Реализуйте функцию to_Timur() с помощью функции partial(), которая принимает один аргумент:
+
+text — содержание письма
+
+Функция должна отправлять письмо пользователю с именем Тимур на адрес timyrik20@beegeek.ru 
+с содержанием text.
+
+2. Реализуйте функцию send_an_invitation() с помощью функции partial(), 
+которая принимает два аргумента в следующем порядке:
+
+name — имя
+email_address — адрес электронной почты
+
+Функция должна отправлять письмо на имя name и на адрес email_address со следующим содержанием:
+
+Школа BEEGEEK приглашает Вас на новый курс по программированию на языке Python. тутут....
+
+Примечание 1. Функции to_Timur() и send_an_invitation() должны являться partial объектами.
+
+Примечание 2. В тестирующую систему сдайте программу, содержащую только необходимые 
+функции to_Timur() и send_an_invitation(), но не код, вызывающий их.
+
+Примечание 3. Тестовые данные доступны по ссылкам:
+
+Архив с тестами
+https://stepik.org/media/attachments/lesson/751476/tests_3130314.zip
+GitHub
+https://github.com/python-generation/Professional/tree/main/Module_9/Module_9.9/Module_9.9.11
+
+Sample Input 1:
+print(to_Timur('когда курс?'))
+Sample Output 1:
+В письме для Тимур на адрес timyrik20@beegeek.ru сказано следующее: когда курс?
+
+Sample Input 2:
+print(send_an_invitation("Тимур", "timyrik20@beegeek.ru"))
+Sample Output 2:
+В письме для Тимур на адрес timyrik20@beegeek.ru сказано следующее: 
+Школа BEEGEEK приглашает Вас на новый курс по программированию на языке Python. тутут....
 '''
-from functools import wraps
+from functools import partial
 
-pass
+def send_email(name, email, text):
+
+    return f'В письме для {name} на адрес {email} сказано следующее: {text}'
+
+to_Timur = partial(send_email, "Тимур", "timyrik20@beegeek.ru")
+
+send_an_invitation = partial(send_email, text="Школа BEEGEEK приглашает Вас на новый курс по программированию на языке Python. тутут....")
 
 if __name__ == '__main__':
-    pass
+    print(to_Timur('когда курс?'))
+
+    print(send_an_invitation("Тимур", "timyrik20@beegeek.ru"))
--- Python. Поколение Python - курс для профессионалов/Module 009/init-14.py
+++ Python. Поколение Python - курс для профессионалов/Module 009/init-14.py
@@ -1,5 +1,145 @@
 print('------------------')
 
-pass
+def multiply(a, b):
+    return a * b
+
+print(multiply(2, 3))
+
+def double(num):
+    return multiply(2, num)
+ 
+def triple(num):
+    return multiply(3, num)
+
+print('------------------')
+
+from functools import partial
+
+def multiply(a, b):
+    return a * b
+
+double = partial(multiply, 2)
+triple = partial(multiply, 3)
+
+print(double(5))        # 2 * 5
+print(triple(10))       # 3 * 10
+
+print('------------------')
+
+# print(double(5, 6))
+
+print('------------------')
+
+multiply_two_and_five = partial(multiply, 2, 5)
+# вызываем уже без аргументов
+print(multiply_two_and_five())  
+
+print('------------------')
+
+print(int('123'))
+print(int('123', base=5))
+print(int('1001', base=2))
+print(int('A12B', base=16))
+
+print('------------------')
+
+from functools import partial
+
+basetwo = partial(int, base=2)
+
+print(basetwo('101'))
+print(basetwo('1000'))
+print(basetwo('11111'))
+
+print('------------------')
+
+from functools import partial
+
+def pretty_print(text, symbol, count):
+    print(symbol * count)
+    print(text)
+    print(symbol * count)
+
+star_pretty_print = partial(pretty_print, 'Hi!!!', symbol='*')
+
+star_pretty_print(count=7)
+
+print(star_pretty_print.args)
+print(star_pretty_print.keywords)
+
+star_pretty_print.func('Исходная функция', symbol='~', count=20)
+
+print('------------------')
+
+from functools import partial
+
+def multiply(a, b):
+    '''Функция перемножает два числа и возвращает вычисленное значение.'''
+    return a * b
+
+double = partial(multiply, 2)
+
+print(double.func.__name__)
+print(double.func.__doc__)
+
+print('------------------')
+
+from functools import partial, update_wrapper
+
+def multiply(a, b):
+    '''Функция перемножает два числа и возвращает вычисленное значение.'''
+    return a * b
+
+double = partial(multiply, 2)
+# копируем информацию из функции multiply в partial объект double
+update_wrapper(double, multiply)   
+
+print(double.__name__)
+print(double.__doc__)
+
+print('------------------')
+
+from functools import partial
+
+def multiply(a, b):
+    return a * b
+
+double = partial(multiply, 2)
+
+print(type(double))
+
+print('------------------')
+
+from functools import update_wrapper
+
+def my_func():
+    """Wrapped function."""
+    return 'This original func'
+
+def wrapper_func():
+    """Wrapper function."""
+    return 'This wrapper func'
+
+print(wrapper_func.__name__) # 'wrapper_func'
+print(wrapper_func.__doc__) # 'Wrapper function.'
+print(wrapper_func()) # This wrapper func
+wrapper_func = update_wrapper(wrapper_func, my_func)
+print(wrapper_func.__name__) # 'my_func'
+print(wrapper_func.__doc__) # 'Wrapped function.'
+print(wrapper_func()) # This wrapper func
+print(wrapper_func.__wrapped__()) # This original func
+
+print('------------------')
+
+# from functools import partial
+
+# def add(a, b):
+#     '''documentation'''
+#     return a + b
+
+# add_one = partial(add, 1)
+
+# print(add_one.__name__)
+# print(add_one.__doc__)
 
 print('------------------')
