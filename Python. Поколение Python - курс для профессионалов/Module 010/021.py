d2d7525bee56455af17c351b4f55cfd710af033c
Max Ramanenka (Red Alert) <i@mer-group.ru (mer-group@yandex.ru)>
2023-07-14 13:38:03 +0300

    add 021 py-proff m10

--- Python. Поколение Python - курс для профессионалов/Module 010/021.py
+++ Python. Поколение Python - курс для профессионалов/Module 010/021.py
@@ -37,7 +37,45 @@ print(*letters)
 Sample Output 2:
 a b c d e f g h i j k l m n o p q r s t u v w x y z a b
 '''
-pass
+class Alphabet:
+    def __init__(self, language: str) -> None:
+        self.language = language
+
+        self.index_ru_start = ord('а')-1
+        self.index_ru_start_copy = ord('а')-1
+        self.index_ru_end = ord('я')
+
+        self.index_en_start = ord('a')-1
+        self.index_en_start_copy = ord('a')-1
+        self.index_en_end = ord('z')
+
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        if 'ru' == self.language:
+            self.index_ru_start += 1
+            if self.index_ru_start > self.index_ru_end:
+                self.index_ru_start = self.index_ru_start_copy
+                self.index_ru_start += 1
+            return chr(self.index_ru_start)
+        else:
+            self.index_en_start += 1
+            if self.index_en_start > self.index_en_end:
+                self.index_en_start = self.index_en_start_copy
+                self.index_en_start += 1
+            return chr(self.index_en_start)
 
 if __name__ == '__main__':
-    pass
+    ru_alpha = Alphabet('ru')
+    print(next(ru_alpha))
+    print(next(ru_alpha))
+    print(next(ru_alpha))
+
+    en_alpha = Alphabet('ru')
+    letters = [next(en_alpha) for _ in range(40)]
+    print(*letters)
+
+    en_alpha = Alphabet('en')
+    letters = [next(en_alpha) for _ in range(28)]
+    print(*letters)
