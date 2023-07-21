cd0404fd716e44b97905ade553e3d1bc772cc288
Max Ramanenka (Red Alert) <i@mer-group.ru (mer-group@yandex.ru)>
2023-07-21 11:42:01 +0300

    add 029 py-proff m10

---
+++ Python. Поколение Python - курс для профессионалов/Module 010/030.py
@@ -0,0 +1,45 @@
+'''
+Функция palindromes()
+Реализуйте генераторную функцию palindromes(), которая не принимает никаких аргументов.
+
+Функция должна возвращать генератор, порождающий бесконечную последовательность натуральных 
+чисел-палиндромов.
+
+Примечание 1. Число-палиндром — число, которое читается одинаково как справа налево, 
+так и слева направо.
+
+Примечание 2. В тестирующую систему сдайте программу, содержащую только необходимую 
+генераторную функцию palindromes(), но не код, вызывающий ее.
+
+Примечание 3. Тестовые данные доступны по ссылкам:
+
+Архив с тестами
+https://stepik.org/media/attachments/lesson/640048/tests_3139059.zip
+GitHub
+https://github.com/python-generation/Professional/tree/main/Module_10/Module_10.5/Module_10.5.26
+
+Sample Input 1:
+generator = palindromes()
+print(next(generator))
+print(next(generator))
+print(next(generator))
+Sample Output 1:
+1
+2
+3
+
+Sample Input 2:
+generator = palindromes()
+numbers = [next(generator) for _ in range(30)]
+print(*numbers)
+Sample Output 2:
+1 2 3 4 5 6 7 8 9 11 22 33 44 55 66 77 88 99 101 111 121 131 141 151 161 171 181 191 202 212
+'''
+
+from __future__ import annotations
+from _collections_abc import Generator
+
+pass
+            
+if __name__ == '__main__':
+    pass
--- Python. Поколение Python - курс для профессионалов/Module 010/init-7.py
+++ Python. Поколение Python - курс для профессионалов/Module 010/init-7.py
@@ -1,5 +1,66 @@
 print('------------------')
 
-pass
+def get_data():
+    for num in range(5):
+        yield num
+    for char in 'ABC':
+        yield char
+
+for i in get_data():
+    print(i)
+
+print('------------------')
+
+def get_data():
+    yield from range(5)
+    yield from 'ABC'
+
+print('------------------')
+
+def chain(*iterables):
+    for it in iterables:
+        for value in it:
+            yield value
+
+for i in chain('AB', [1, 2], (4, 5), {'name': 'Timur', 'age': 29}):
+    print(i, end=' ')
+
+print()
+print('------------------')
+
+def chain(*iterables):
+    for it in iterables:
+        yield from it
+
+print('------------------')
+
+def generator2():
+    yield 'Red'
+    yield 'Blue'
+
+def generator1():
+    yield 'Green'
+    # запрашиваем значение из субгенератора
+    yield from generator2()            
+    yield 'Yellow'
+    yield 'Black'
+
+for color in generator1():
+    print(color, end=' ')
+
+print()
+
+print('------------------')
+
+def numbers(start):
+    if not isinstance(start, int):
+        raise TypeError('Аргументом должно быть целое число')
+    yield start
+    yield from numbers(start + 1)
+
+for index, number in enumerate(numbers(3)):
+    if index > 5:
+        break
+    print(number)
 
 print('------------------')
