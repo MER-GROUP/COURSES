50c3c91194bcd64d30a1333d401100bd16c27b9c
Max Ramanenka (Red Alert) <i@mer-group.ru (mer-group@yandex.ru)>
2023-07-28 13:41:37 +0300

    add 037 py-proff m10

---
+++ Python. Поколение Python - курс для профессионалов/Module 010/037.py
@@ -0,0 +1,56 @@
+'''
+Person
+
+Вам доступен именованный кортеж Person, который содержит данные о человеке. 
+Первым элементом именованного кортежа является имя и фамилия человека, 
+вторым — национальность, третьим — пол, четвертым — год рождения, 
+пятым — год смерти. Если человек жив, год смерти считается равным 0. 
+Также доступен список persons, содержащий эти кортежи.
+
+Дополните приведенный ниже код с использованием конвейеров генераторов, 
+чтобы он вывел имя и фамилию самого молодого живого мужчины (male) из Швеции (Swedish).
+
+Примечание 1. Пример вывода:
+
+Goran Aslin
+Примечание 2. Гарантируется, что искомый человек единственный.
+
+from collections import namedtuple
+
+Person = namedtuple('Person', ['name', 'nationality', 'sex', 'birth', 'death'])
+
+persons = [Person('E. M. Ashe', 'American', 'male', 1867, 1941),
+           Person('Goran Aslin', 'Swedish', 'male', 1980, 0),
+           Person('Erik Gunnar Asplund', 'Swedish', 'male', 1885, 1940),
+           Person('Genevieve Asse', 'French', 'female', 1949, 0),
+           Person('Irene Adler', 'Swedish', 'female', 2005, 0),
+           Person('Sergio Asti', 'Italian', 'male', 1926, 0),
+           Person('Olof Backman', 'Swedish', 'male', 1999, 0),
+           Person('Alyson Hannigan', 'Swedish', 'female', 1940, 1987),
+           Person('Dana Atchley', 'American', 'female', 1941, 2000),
+           Person('Monika Andersson', 'Swedish', 'female', 1957, 0),
+           Person('Shura_Stone', 'Russian', 'male', 2000, 0),
+           Person('Jon Bale', 'Swedish', 'male', 2000, 0)]
+'''
+from collections import namedtuple
+
+Person = namedtuple('Person', ['name', 'nationality', 'sex', 'birth', 'death'])
+
+persons = [Person('E. M. Ashe', 'American', 'male', 1867, 1941),
+           Person('Goran Aslin', 'Swedish', 'male', 1980, 0),
+           Person('Erik Gunnar Asplund', 'Swedish', 'male', 1885, 1940),
+           Person('Genevieve Asse', 'French', 'female', 1949, 0),
+           Person('Irene Adler', 'Swedish', 'female', 2005, 0),
+           Person('Sergio Asti', 'Italian', 'male', 1926, 0),
+           Person('Olof Backman', 'Swedish', 'male', 1999, 0),
+           Person('Alyson Hannigan', 'Swedish', 'female', 1940, 1987),
+           Person('Dana Atchley', 'American', 'female', 1941, 2000),
+           Person('Monika Andersson', 'Swedish', 'female', 1957, 0),
+           Person('Shura_Stone', 'Russian', 'male', 2000, 0),
+           Person('Jon Bale', 'Swedish', 'male', 2000, 0)]
+
+live_men = (i for i in persons if not i.death)
+only_men = (i for i in live_men if 'male' == i.sex)
+swe_men = (i for i in only_men if 'Swedish' == i.nationality)
+yong_men = max(swe_men, key=lambda x: x.birth)
+print(yong_men.name)
