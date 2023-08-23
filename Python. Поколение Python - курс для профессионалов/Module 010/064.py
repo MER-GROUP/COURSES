f68bf56e548c261abc7134d15728b7e231f35d40
Max Ramanenka (Red Alert) <i@mer-group.ru (mer-group@yandex.ru)>
2023-08-23 12:56:28 +0300

    add 064-066 py-proff m10

--- Python. Поколение Python - курс для профессионалов/Module 010/064.py
+++ Python. Поколение Python - курс для профессионалов/Module 010/064.py
@@ -33,7 +33,6 @@ from _collections_abc import Generator, Iterator, Iterable, Callable
 import itertools as it
 
 from collections import namedtuple
-from itertools import groupby
 
 Person = namedtuple('Person', ['name', 'age', 'height'])
 
@@ -41,3 +40,8 @@ persons = [Person('Tim', 63, 193), Person('Eva', 47, 158),
            Person('Mark', 71, 172), Person('Alex', 45, 193),
            Person('Jeff', 63, 193), Person('Ryan', 41, 184),
            Person('Ariana', 28, 158), Person('Liam', 69, 193)]
+
+persons_sorted = sorted(persons, key=lambda x: (x[2], x[0]))
+persons_groupby = it.groupby(persons_sorted, key=lambda x: x[2])
+for key, group in persons_groupby:
+    print(f'{key}: {", ".join(i[0] for i in group)}')
---
+++ Python. Поколение Python - курс для профессионалов/Module 010/065.py
@@ -0,0 +1,50 @@
+'''
+Students
+
+Вам доступен именованный кортеж Student, который содержит данные об ученике. Первым элементом 
+именованного кортежа является фамилия ученика, вторым — имя, третьим — класс. Также доступен 
+список students, содержащий эти кортежи.
+
+Дополните приведенный ниже код, чтобы он вывел наиболее часто встречаемое имя среди учеников 
+из данного списка.
+
+Примечание. Гарантируется, что искомое имя единственное.
+
+from collections import namedtuple
+from itertools import groupby
+
+Student = namedtuple('Student', ['surname', 'name', 'grade'])
+
+students = [Student('Гагиев', 'Александр', 10), Student('Дедегкаев', 'Илья', 11), Student('Кодзаев', 'Георгий', 10),
+            Student('Набокова', 'Алиса', 11), Student('Кораев', 'Артур', 10), Student('Шилин', 'Александр', 11),
+            Student('Уртаева', 'Илина', 11), Student('Салбиев', 'Максим', 10), Student('Капустин', 'Илья', 11),
+            Student('Гудцев', 'Таймураз', 11), Student('Перчиков', 'Максим', 10), Student('Чен', 'Илья', 11),
+            Student('Елькина', 'Мария', 11),Student('Макоев', 'Руслан', 11), Student('Албегов', 'Хетаг', 11),
+            Student('Щербак', 'Илья', 10), Student('Идрисов', 'Баграт', 11), Student('Гапбаев', 'Герман', 10),
+            Student('Цивинская', 'Анна', 10), Student('Туткевич', 'Юрий', 11), Student('Мусиков', 'Андраник', 11),
+            Student('Гадзиев', 'Георгий', 11), Student('Белов', 'Юрий', 11), Student('Акоева', 'Диана', 11),
+            Student('Денисов', 'Илья', 11), Student('Букулова', 'Диана', 10), Student('Акоева', 'Лера', 11)]
+'''
+from __future__ import annotations
+from _collections_abc import Generator, Iterator, Iterable, Callable
+import itertools as it
+
+from collections import namedtuple
+from itertools import groupby
+
+Student = namedtuple('Student', ['surname', 'name', 'grade'])
+
+students = [Student('Гагиев', 'Александр', 10), Student('Дедегкаев', 'Илья', 11), Student('Кодзаев', 'Георгий', 10),
+            Student('Набокова', 'Алиса', 11), Student('Кораев', 'Артур', 10), Student('Шилин', 'Александр', 11),
+            Student('Уртаева', 'Илина', 11), Student('Салбиев', 'Максим', 10), Student('Капустин', 'Илья', 11),
+            Student('Гудцев', 'Таймураз', 11), Student('Перчиков', 'Максим', 10), Student('Чен', 'Илья', 11),
+            Student('Елькина', 'Мария', 11),Student('Макоев', 'Руслан', 11), Student('Албегов', 'Хетаг', 11),
+            Student('Щербак', 'Илья', 10), Student('Идрисов', 'Баграт', 11), Student('Гапбаев', 'Герман', 10),
+            Student('Цивинская', 'Анна', 10), Student('Туткевич', 'Юрий', 11), Student('Мусиков', 'Андраник', 11),
+            Student('Гадзиев', 'Георгий', 11), Student('Белов', 'Юрий', 11), Student('Акоева', 'Диана', 11),
+            Student('Денисов', 'Илья', 11), Student('Букулова', 'Диана', 10), Student('Акоева', 'Лера', 11)]
+
+students.sort(key=lambda x: x.name)
+students_groupby = groupby(students, key=lambda x: x.name)
+name = max(students_groupby, key=lambda x: len(list(x[1])))[0]
+print(name)
