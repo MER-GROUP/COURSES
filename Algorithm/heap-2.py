c6e4a6786afa5e2dc68f2eeb3fbe2bb0c5ed7d01
Max Ramanenka (Red Alert) <i@mer-group.ru (mer-group@yandex.ru)>
2023-09-21 21:46:32 +0300

    init-29 heap algo

---
+++ Algorithm/heap-2.py
@@ -0,0 +1,69 @@
+###############################################################################################
+# реализация класа min-кучи
+class BinHeap:
+    def __init__(self):
+        self.heapList = [0]
+        self.currentSize = 0
+
+    # переместить элемент до самого верха дерева
+    def percUp(self,i):
+        while i // 2 > 0:
+          if self.heapList[i] < self.heapList[i // 2]:
+             tmp = self.heapList[i // 2]
+             self.heapList[i // 2] = self.heapList[i]
+             self.heapList[i] = tmp
+          i = i // 2
+
+    # вставить элемент
+    def insert(self,k):
+      self.heapList.append(k)
+      self.currentSize = self.currentSize + 1
+      self.percUp(self.currentSize)
+
+    # переместить элемент до самого низа дерева
+    def percDown(self,i):
+      while (i * 2) <= self.currentSize:
+          mc = self.minChild(i)
+          if self.heapList[i] > self.heapList[mc]:
+              tmp = self.heapList[i]
+              self.heapList[i] = self.heapList[mc]
+              self.heapList[mc] = tmp
+          i = mc
+
+    # минимальный узел-ребенок
+    def minChild(self,i):
+      if i * 2 + 1 > self.currentSize:
+          return i * 2
+      else:
+          if self.heapList[i*2] < self.heapList[i*2+1]:
+              return i * 2
+          else:
+              return i * 2 + 1
+
+    # удаление узла из дерева
+    def delMin(self):
+      retval = self.heapList[1]
+      self.heapList[1] = self.heapList[self.currentSize]
+      self.currentSize = self.currentSize - 1
+      self.heapList.pop()
+      self.percDown(1)
+      return retval
+
+    # создание min-кучи
+    def buildHeap(self,alist):
+      i = len(alist) // 2
+      self.currentSize = len(alist)
+      self.heapList = [0] + alist[:]
+      while (i > 0):
+          self.percDown(i)
+          i = i - 1
+###############################################################################################
+bh = BinHeap()
+bh.buildHeap([9,5,6,2,3])
+
+print(bh.delMin())
+print(bh.delMin())
+print(bh.delMin())
+print(bh.delMin())
+print(bh.delMin())
+###############################################################################################
