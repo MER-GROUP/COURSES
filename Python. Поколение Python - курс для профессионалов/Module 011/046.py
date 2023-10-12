3048fb24ce15e29861aa734b5f8968f098a90ca2
Max Ramanenka (Red Alert) <i@mer-group.ru (mer-group@yandex.ru)>
2023-10-12 13:41:57 +0300

    add 045 py-proff m11

---
+++ Python. Поколение Python - курс для профессионалов/Module 011/046.py
@@ -0,0 +1,54 @@
+'''
+Подслова
+
+Напишите программу, которая принимает на вход строку текста и некоторое 
+слово и определяет, сколько раз данное слово встречается как подслово в введенном тексте.
+
+Формат входных данных
+На вход программе на первой строке подается текст, на второй — слово.
+
+Формат выходных данных
+Программа должна определить, сколько раз данное слово встречается как подслово 
+в введенном тексте, и вывести полученный результат.
+
+Примечание 1. Словом будем считать последовательность символов, соответствующих \w, 
+окруженную символами, соответствующими \W. Подсловом же будет являться последовательность 
+символов, соответствующих \w, окруженную символами, соответствующими \w. 
+Например, is является подсловом optimist, а age не является подсловом ageless.
+
+Примечание 2. Программа должна учитывать регистр. То есть, например, 
+слова Python и python считаются разными.
+
+Примечание 3. Тестовые данные доступны по ссылкам:
+
+Архив с тестами
+https://stepik.org/media/attachments/lesson/680265/tests_2830471.zip
+GitHub
+https://github.com/python-generation/Professional/tree/main/Module_11/Module_11.7/Module_11.7.11
+
+Sample Input 1:
+existing pessimist optimist this is
+is
+Sample Output 1:
+3
+
+Sample Input 2:
+I love Python very much, what about me hahah
+ha
+Sample Output 2:
+2
+
+Sample Input 3:
+thdbakru rubabadjso babadirnid iehedba  ibebibeb duabafbf
+ba
+Sample Output 3:
+5
+'''
+import __future__
+# from _collections_abc import Sequence
+import re
+from sys import stdin
+
+if __name__ == '__main__':
+    stdin = open(file='046-test.csv', mode='rt', encoding='utf-8', newline='')
+    pattern = r''
--- Python. Поколение Python - курс для профессионалов/Module 011/init-5.py
+++ Python. Поколение Python - курс для профессионалов/Module 011/init-5.py
@@ -1,5 +1,64 @@
 print('------------------')
 
-pass
+import re
+
+text = 'ул. Часовая, дом № 25, корпус 2, квартира 69'
+result = re.findall('\d+', text)
+
+print(result)
+
+print('------------------')
+
+import re
+
+result = re.findall('#(\w+)#', '#foo#.#bar#.#baz#')
+
+print(result)
+
+print('------------------')
+
+import re
+
+result1 = re.findall('(\w+),(\w+)', 'foo,bar,baz,qux,quux,corge')
+result2 = re.findall('(\w+),(\w+),(\w+)', 'foo,bar,baz,qux,quux,corge')
+
+print(result1)
+print(result2)
+
+print('------------------')
+
+import re
+
+result = re.findall('(\w+),(\w+),(\w+)?', 'foo,bar,')
+
+print(result)
+
+print('------------------')
+
+import re
+
+result = re.findall('(\w+),(\w+),(\w+)', 'foo,bar,')
+
+print(result)
+
+print('------------------')
+
+import re
+
+text = 'ул. Часовая, дом № 25, корпус 2, квартира 69'
+result = re.finditer('\d+', text)
+
+print(type(result))
+print(list(result))
+
+print('------------------')
+
+import re
+
+result = re.finditer('#(\w+)#', '#foo#.#bar#.#baz#')
+
+for match in result:
+    print(match)
+    print(match.group(), match.group(1))
 
 print('------------------')
