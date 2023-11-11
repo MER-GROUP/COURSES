0ca65b8e959c68820085b10ae926da1df90a4e5e
Max Ramanenka (Red Alert) <i@mer-group.ru (mer-group@yandex.ru)>
2023-11-11 13:19:52 +0300

    add 063 py-proff m11

--- Python. Поколение Python - курс для профессионалов/Module 011/063.py
+++ Python. Поколение Python - курс для профессионалов/Module 011/063.py
@@ -1,20 +1,66 @@
 '''
+Сумма чисел
 
+Напишите программу, которая складывает все натуральные числа в строке, 
+находящиеся в указанном диапазоне индексов.
+
+Формат входных данных
+На вход программе сначала подаются два целых числа aa и b, больших или 
+равных 0, разделенные пробелом, а затем — строка.
+
+Формат выходных данных
+Программа должна вывести сумму всех натуральных чисел в введенной строке, 
+находящихся в диапазоне индексов от a (включительно) до b (не включительно). 
+Если в указанном диапазоне нет ни одного числа, программа должна вывести 0.
+
+Примечание. Тестовые данные доступны по ссылкам:
+
+Архив с тестами
+https://stepik.org/media/attachments/lesson/699083/tests_2916589.zip
+GitHub
+https://github.com/python-generation/Professional/tree/main/Module_11/Module_11.9/Module_11.9.19
+
+Sample Input 1:
+0 5
+11:20 a.m. - 12:00 p.m
+Sample Output 1:
+31
+
+Sample Input 2:
+0 10
+Нет ни одного числа в этой строке
+Sample Output 2:
+0
+
+Sample Input 3:
+0 100
+Нет ни одного числа в этой строке
+Sample Output 3:
+0
 '''
 import __future__
 # from _collections_abc import Sequence
 import re
 from sys import stdin
 
-def func(string: str) -> str:
-    ...
+def func(string: str, text: str) -> int:
+    a, b = map(int, string.split())
+    pattern_compile = re.compile(fr'\d+')
+    match = pattern_compile.findall(text, pos=a, endpos=b)
+    return sum(
+        map(
+            int,
+            match
+        ),
+        0
+    )
 
 if __name__ == '__main__':
-    stdin = open(file='063-test.csv', mode='rt', encoding='utf-8', newline='')
-    # sentense = map(str, stdin)
-    sentense = stdin.read()
-    print(sentense) # test
-    print('------') # test
-    pattern = rf''
+    # stdin = open(file='063-test.csv', mode='rt', encoding='utf-8', newline='')
+    # # sentense = map(str, stdin)
+    # sentense = stdin.read()
+    # print(sentense) # test
+    # print('------') # test
+    # pattern = rf''
 
-    pass
+    print(func(input(), input()))
--- Python. Поколение Python - курс для профессионалов/Module 011/init-8.py
+++ Python. Поколение Python - курс для профессионалов/Module 011/init-8.py
@@ -1,5 +1,79 @@
 print('------------------')
 
-pass
+import re
+
+regex_obj = re.compile('\d+')
+text = 'ул. Часовая, дом № 25, корпус 2, квартира 69'
+result = re.findall(regex_obj, text)
+
+print(result)
+
+print('------------------')
+
+import re
+
+regex_obj = re.compile('\d+')
+text = 'ул. Часовая, дом № 25, корпус 2, квартира 69'
+result = regex_obj.findall(text)
+
+print(result)
+
+print('------------------')
+
+import re
+
+regex_obj = re.compile('ba[rz]', flags=re.I)
+
+result1 = re.search('ba[rz]', 'FOOBARBAZ', flags=re.I)
+result2 = re.search(regex_obj, 'FOOBARBAZ')
+result3 = regex_obj.search('FOOBARBAZ')
+
+print(result1)
+print(result2)
+print(result3)
+
+print('------------------')
+
+import re
+
+s1, s2, s3, s4 = 'foo.bar', 'foo123bar', 'baz99', 'qux & grault'
+
+print(re.search('\d+', s1))
+print(re.search('\d+', s2))
+print(re.search('\d+', s3))
+print(re.search('\d+', s4))
+
+print('------------------')
+
+import re
+
+s1, s2, s3, s4 = 'foo.bar', 'foo123bar', 'baz99', 'qux & grault'
+
+regex_obj = re.compile('\d+')
+
+print(regex_obj.search(s1))
+print(regex_obj.search(s2))
+print(regex_obj.search(s3))
+print(regex_obj.search(s4))
+
+print('------------------')
+
+import re
+
+regex_obj = re.compile('\d+')
+text = 'foo12345barbaz'
+
+print(regex_obj.search(text))
+print(regex_obj.search(text, pos=4))
+print(regex_obj.search(text, endpos=7))
+print(regex_obj.search(text, pos=4, endpos=7))
+
+print('------------------')
+
+import re
+
+regex_obj = re.compile('\d')
+
+print(type(regex_obj))
 
 print('------------------')
