5f7c7b9873958af5318bcbe86be6c0746b81ece9
Max Ramanenka (Red Alert) <i@mer-group.ru (mer-group@yandex.ru)>
2023-11-23 10:59:37 +0300

    add 118 numpy m3.15

--- Numpy и анализ данных/module-3/118.py
+++ Numpy и анализ данных/module-3/118.py
@@ -28,7 +28,7 @@ Sample Output:
 '''
 import numpy as np
 from sys import stdin
-stdin = open(file='118.csv', mode='rt', encoding='utf-8', newline='')
+# stdin = open(file='118.csv', mode='rt', encoding='utf-8', newline='')
 
 if __name__ == '__main__':
     # arr = np.fromstring(
@@ -40,4 +40,6 @@ if __name__ == '__main__':
     # print(type(arr)) # test #
 
     arr1, arr2 = (np.fromstring(string=i, dtype=int, sep=' ') for i in stdin)
-    print(arr1, arr2, sep='\n')
+    # print(arr1, arr2, sep='\n')
+
+    print(*np.maximum(arr1, arr2))
