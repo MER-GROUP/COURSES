f264db2ea962580fb2c601281dbe58cc0e269936
Max Ramanenka (Red Alert) <i@mer-group.ru (mer-group@yandex.ru)>
2023-11-24 10:15:59 +0300

    init 120 numpy m3.15

--- Numpy и анализ данных/module-3/120.py
+++ Numpy и анализ данных/module-3/120.py
@@ -1,20 +1,36 @@
 '''
+Экспоненциальный свинг хвоста
 
+Представьте, что вы являетесь собакой и вы хотите вычислить, сколько раз в день 
+вы виляете хвостом. Для этого вы записываете свои данные в массив tail_wags_per_hour 
+содержащий количество раз, которое вы виляете хвостом за каждый час в течение дня. 
+Затем вы применяете функцию
+
+\(e^x - 1\)
+
+к этому массиву и получаете результат, который называется "экспоненциальный свинг хвоста". 
+Выведите полученный результат, округленный до двух знаков, на экран.
+
+Sample Input:
+5 9 7 6 0 8 0 1 1 3 5 13 6 5 10 13 5 2 6 1 11 7 8 9
+
+Sample Output:
+147.41 8102.08 1095.63 402.43 0.0 2979.96 0.0 1.72 1.72 19.09 147.41 442412.39 402.43 147.41 22025.47 442412.39 147.41 6.39 402.43 1.72 59873.14 1095.63 2979.96 8102.08
 '''
 import numpy as np
 from sys import stdin
 stdin = open(file='120.csv', mode='rt', encoding='utf-8', newline='')
 
 if __name__ == '__main__':
-    # arr = np.fromstring(
-    #     string=stdin.read(),
-    #     dtype=float,
-    #     sep = ' '
-    # )
-    # print(arr) # test #
-    # print(type(arr)) # test #
+    arr = np.fromstring(
+        string=stdin.read(),
+        dtype=int,
+        sep = ' '
+    )
+    print(arr) # test #
+    print(type(arr)) # test #
 
-    arr1, arr2 = (np.fromstring(string=i, dtype=float, sep=' ') for i in stdin)
-    print(arr1, arr2, sep='\n')
+    # arr1, arr2 = (np.fromstring(string=i, dtype=float, sep=' ') for i in stdin)
+    # print(arr1, arr2, sep='\n')
 
     pass
