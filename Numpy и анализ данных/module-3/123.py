bdf3918aa0c9d73d40b3d26939e2f85c1f9e9ef8
Max Ramanenka (Red Alert) <i@mer-group.ru (mer-group@yandex.ru)>
2023-11-30 10:24:51 +0300

    add 123 numpy m3.15

--- Numpy и анализ данных/module-3/123.py
+++ Numpy и анализ данных/module-3/123.py
@@ -35,7 +35,7 @@ Sample Output:
 '''
 import numpy as np
 from sys import stdin
-stdin = open(file='123.csv', mode='rt', encoding='utf-8', newline='')
+# stdin = open(file='123.csv', mode='rt', encoding='utf-8', newline='')
 
 if __name__ == '__main__':
 #     arr = np.fromstring(
@@ -48,4 +48,6 @@ if __name__ == '__main__':
 #     print(type(arr)) # test #
 
     arr1, arr2 = (np.fromstring(string=i, dtype=int, sep=' ') for i in stdin)
-    print(arr1, arr2, sep='\n')
+    # print(arr1, arr2, sep='\n')
+
+    print(*np.lcm(arr1, arr2))
