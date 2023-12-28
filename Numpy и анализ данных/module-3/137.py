68d16786b43fe8c7805022633bb95babf0c0a477
Max Ramanenka (Red Alert) <i@mer-group.ru (mer-group@yandex.ru)>
2023-12-28 09:48:12 +0300

    add 137 numpy m3.16

--- Numpy и анализ данных/module-3/137.py
+++ Numpy и анализ данных/module-3/137.py
@@ -17,7 +17,7 @@ Sample Output:
 '''
 import numpy as np
 from sys import stdin
-stdin = open(file='137.csv', mode='rt', encoding='utf-8', newline='')
+# stdin = open(file='137.csv', mode='rt', encoding='utf-8', newline='')
 
 if __name__ == '__main__':
 #     arr = np.fromstring(
@@ -34,7 +34,12 @@ if __name__ == '__main__':
 
     arr1, arr2, *_ = (np.fromstring(string=i, dtype=int, sep=' ') for i in stdin)
     # arr1, *_ = (np.fromstring(string=i, dtype=int, sep=' ') for i in stdin)
-    print(arr1) # test
-    print(arr2) # test
+    # print(arr1) # test
+    # print(arr2) # test
 
-    pass
+    print(
+        *np.where(
+            (arr1<=arr2[0]) | (arr1 >= arr2[1]), 
+            arr1.mean(), 
+            arr1).round(1)
+    )
