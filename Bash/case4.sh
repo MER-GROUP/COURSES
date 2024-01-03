42d4763e0eedbfdf1b0d7d37472c01f5e65fc390
Max Ramanenka (Red Alert) <i@mer-group.ru (mer-group@yandex.ru)>
2024-01-03 21:28:11 +0300

    add case3 bash

---
+++ Bash/case4.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+echo "##################################"
+
+# case ${filename} in
+case ${1} in
+    ./* )
+        echo -ne "local"
+    ;&
+    [^/]* )
+        echo -ne "relative\n"
+    ;;&
+    /* )
+        echo -ne "absolute"
+    ;&
+    */* )
+        echo -ne "pathname"
+    ;;
+    * )
+        echo -ne "filename"
+    ;;
+esac
+
+echo "##################################"
