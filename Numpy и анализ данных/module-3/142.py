053f2829b7afae3902a5e40489a6b2516a7332ab
Max Ramanenka (Red Alert) <i@mer-group.ru (mer-group@yandex.ru)>
2024-01-19 15:10:17 +0300

    init 142 numpy m3.17

--- Numpy и анализ данных/module-3/142.py
+++ Numpy и анализ данных/module-3/142.py
@@ -1,5 +1,16 @@
 '''
+Cреднее значение
 
+Рассчитать среднее значение роста студентов в группе на основе их ростов, 
+представленных в виде одномерного массива, который поступает во входной строке. 
+Вывести ответ в формате: "Средний рост студентов в группе: х.х см", где х.х искомые 
+значения. Результат округляйте до одного знака после запятой, кавычки не используйте.
+
+Sample Input:
+187.0 186.7 220.9 176.0 166.0 171.7 201.7 180.5 133.4 146.3 237.5 141.0 215.3 181.5 166.2 126.2 137.6 189.1 219.5 160.5 217.7 129.1 166.6 159.9 134.2 201.7 153.0
+
+Sample Output:
+Средний рост студентов в группе: 174.3 см
 '''
 import numpy as np
 from sys import stdin
@@ -19,10 +30,10 @@ if __name__ == '__main__':
     # print(arr1, arr2, arr3, sep='\n')
 
     # arr1, arr2, *_ = (np.array(object=i.split(), dtype=str) for i in stdin)
-    arr1, arr2, *_ = (np.fromstring(string=i, dtype=int, sep=' ') for i in stdin)
+    # arr1, arr2, *_ = (np.fromstring(string=i, dtype=int, sep=' ') for i in stdin)
     # arr1, arr2, *_ = (np.array(i.split(), dtype=int) for i in stdin)
-    # arr1, *_ = (np.fromstring(string=i, dtype=int, sep=' ') for i in stdin)
+    arr1, *_ = (np.fromstring(string=i, dtype=float, sep=' ') for i in stdin)
     print(arr1) # test
-    print(arr2) # test
+    # print(arr2) # test
 
     pass
--- Numpy и анализ данных/module-3/init-11.py
+++ Numpy и анализ данных/module-3/init-11.py
@@ -1,5 +1,58 @@
 print('---------------------------')
 
-pass
+data = [10, 15, 20, 30, 25, 30, 20, 15, 10, 5]
+print(data)
+import numpy as np
+hist, bin_edges = np.histogram(data, bins=5)
+
+print(hist) # [1 2 2 2 3] 
+print(bin_edges) # [ 5. 10. 15. 20. 25. 30.]
+
+print('---------------------------')
+
+data = [10, 15, 20, 30, 25, 30, 20, 15, 10, 5]
+import numpy as np 
+percentiles = np.percentile(data, [25, 50, 75]) 
+print(percentiles) # [11.25 17.5 23.75]
+
+print('---------------------------')
+
+a = np.array([1, 2, 3, 4, 5])
+mean_a = np.mean(a)
+print(mean_a)
+
+print('---------------------------')
+
+a = np.array([1, 2, 3, 4, 5])
+median_a = np.median(a)
+print(median_a)
+
+print('---------------------------')
+
+a = np.array([1, 2, 3, 4, 5])
+std_a = np.std(a)
+print(std_a)
+
+print('---------------------------')
+
+a = np.array([1, 2, 3, 4, 5])
+var_a = np.var(a)
+print(var_a)
+
+print('---------------------------')
+
+print(np.mean([3, 5, 7, 9]))
+print(np.std([3, 5, 7, 9]))
+
+print('---------------------------')
+
+print(f'arr = {[1, 2, 3, 4, 5, 6]}')
+print(np.histogram([1, 2, 3, 4, 5, 6], bins=1))
+print(np.histogram([1, 2, 3, 4, 5, 6], bins=2))
+print(np.histogram([1, 2, 3, 4, 5, 6], bins=3))
+print(np.histogram([1, 2, 3, 4, 5, 6], bins=4))
+print(np.histogram([1, 2, 3, 4, 5, 6], bins=5))
+print(np.histogram([1, 2, 3, 4, 5, 6], bins=6))
+print(np.histogram([1, 2, 3, 4, 5, 6], bins=7))
 
 print('---------------------------')
