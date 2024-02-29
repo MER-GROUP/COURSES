f51ce8c98e93b2ef79dcda5b869981c1c88b8551
Max Ramanenka (Red Alert) <i@mer-group.ru (mer-group@yandex.ru)>
2024-02-29 20:53:33 +0300

    add vars6 arifmetic1 bash

---
+++ Bash/vars6.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+echo "###################################"
+
+echo $(echo $(echo $(echo $(echo inside))))
+
+echo "###################################"
+
+echo $(echo $(echo $(echo $(echo inside))))
+echo inside
+
+echo "###################################"
+
+echo `echo \`echo \\\`echo inside\\\`\``
+
+echo "###################################"
+
+echo `echo `echo `echo inside```
+
+echo "###################################"
+
+echo `echo \`echo \\\`echo inside\\\`\``
+
+echo "###################################"
