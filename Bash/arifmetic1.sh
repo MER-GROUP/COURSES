9221533dacc2b1a54230d258512b83e65b326c40
Max Ramanenka (Red Alert) <i@mer-group.ru (mer-group@yandex.ru)>
2024-03-07 21:47:30 +0300

    update-1 arifmetic1 bash

--- Bash/arifmetic1.sh
+++ Bash/arifmetic1.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
 echo "###################################"
 
+echo "Hello 1"
+# sleep 0.25
+# sleep 0.5
+# sleep 2.5
+# sleep $((2 * 0.25)) # error
+# sleep 2 * 0.25 # error
+sleep $(awk 'BEGIN { print (1 * 0.25) }')
+echo "Hello 2 and $(awk 'BEGIN { a = 6; b = 2; print (a + b) }')" 
+echo "Hello 2 and $(awk 'BEGIN { print (1 * 0.25) }')" 
+echo "Hello 2 and $(echo "1 * 0.25" | bc)" 
+echo "Hello 2 and $((1 * 25))" 
+
+echo "###################################"
+
 # ...
 
 echo "###################################"
