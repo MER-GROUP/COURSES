63ce982a1b42b7a00b8905e3e43a928f2b6962f6
Max Ramanenka (Red Alert) <i@mer-group.ru (mer-group@yandex.ru)>
2024-03-09 20:56:18 +0300

    update-58 test balakirev-1 m8

--- Python. Balakirev-1/modole-02/test.py
+++ Python. Balakirev-1/modole-02/test.py
@@ -5465,6 +5465,44 @@ print('##################################')
 
 print('##################################')
 
+# import sys
+# _stdin = sys.stdin
+# sys.stdin = open(file='test.csv', mode='rt', encoding='utf-8', newline='')
+
+# # считывание списка из входного потока
+# # lst_in = sys.stdin.read()
+# # lst_in = sys.stdin.read().strip()
+# # lst_in = int(sys.stdin.read().strip())
+# # lst_in = sys.stdin.read().split()
+# # lst_in = sys.stdin.read().split('\n')
+# # lst_in = list(map(int, sys.stdin.read().split()))
+# # lst_in = list(map(float, sys.stdin.read().split()))
+# # lst_in = map(str.strip, sys.stdin.readlines())
+# # lst_in = list(map(lambda x: list(map(int, x.split())), sys.stdin.readlines()))
+# # lst_in = list(map(lambda x: x.strip().split('='), sys.stdin.readlines()))
+# # lst_in = map(lambda x: x.strip().split('='), sys.stdin.readlines())
+# # lst_in = list(map(str.split, sys.stdin.readlines()))
+# lst_in = list(map(str.strip, sys.stdin.readlines()))
+
+# print(lst_in) # test
+# # print(lst_in.split()) # test
+
+# # здесь продолжайте программу (используйте список lst_in)
+# # arr1, arr2, *_ = (set(map(str.strip, line.split())) for line in lst_in)
+# # arr1, arr2, *_ = lst_in
+# # arr1, *_ = (tuple(map(str.strip, line.split())) for line in lst_in)
+# # arr1 = tuple(int(i) for i in lst_in.split())
+
+# # print(arr1) # test
+# # print(arr2) # test
+
+# sys.stdin = _stdin
+
+# import sys, pprint
+# pprint.pprint(sys.path)
+
+print('##################################')
+
 import sys
 _stdin = sys.stdin
 sys.stdin = open(file='test.csv', mode='rt', encoding='utf-8', newline='')
